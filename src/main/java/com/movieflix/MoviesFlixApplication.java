2f86bb3ffa423737f2b4176119c8273cce263c70
Ish Jaiswal <ishjaiswal7@gmail.com>
2026-01-24 18:36:55 +0530

    Initial commit

---
+++ src/main/java/com/movieflix/MoviesFlixApplication.java
@@ -0,0 +1,13 @@
+package com.movieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesFlixApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/movieflix/controller/FileController.java
@@ -0,0 +1,7 @@
+package com.movieflix.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileController {
+}
