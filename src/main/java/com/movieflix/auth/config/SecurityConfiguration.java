3085681d60bd8f0887c1b87f996c133f7f465d61
Ish Jaiswal <ishjaiswal7@gmail.com>
2026-01-31 00:44:12 +0530

    modified SecurityConfiguration.java

--- src/main/java/com/movieflix/auth/config/SecurityConfiguration.java
+++ src/main/java/com/movieflix/auth/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final AuthFilterService authFilterService;
@@ -25,7 +27,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**")
+                        .requestMatchers("/api/v1/auth/**", "/forgotPassword/**", "/file/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
@@ -36,5 +38,4 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-
 }
